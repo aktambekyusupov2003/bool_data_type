390b8a48142dfc0e74d515d75d2ca7693019e268
aktam <aktam36911@gmail.com>
2023-02-21 16:33:02 +0500

    bool01.dart

--- bin/bool01.dart
+++ bin/bool01.dart
@@ -1,5 +1,5 @@
 /*
-  Create function func
+ Create function func
   Check the logic "they are equal"
   Args:
       a: int
@@ -8,9 +8,22 @@
       bool
   */
 
+void main() {
+  bool dars(int a) {
+    return a > 24;
+    bool func(int a, int b) {
+      return a == b;
+    }
 
-void main() {}
+    void main() {
+      int x = 8;
+      print(x >= 3);
 
-
-
-
+      int a = 6;
+      print(a = 6);
+      int b = 4;
+      print(b != 4);
+      print(func(5, 6));
+    }
+  }
+}
